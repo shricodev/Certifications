befc0a157fcd63e7d73d2ae4224c5609e7c342d6
Shrijal Achärya <76906722+r3alix01@users.noreply.github.com>
2022-01-21 14:33:55 +0530

    Update graded_assessment.py

--- Google - PythonCerts/Using Python To Interact With The Operating System/week-1/graded_assessment.py
+++ Google - PythonCerts/Using Python To Interact With The Operating System/week-1/graded_assessment.py
@@ -5,15 +5,15 @@ from network import *
 import shutil
 import psutil
 def check_disk_usage(disk):
-    """Verifies that there's enough free space on disk"""
     du = shutil.disk_usage(disk)
     free = du.free / du.total * 100
     return free > 20
+
 def check_cpu_usage():
     """Verifies that there's enough unused CPU"""
     usage = psutil.cpu_percent(1)
     return usage <75
-# If there's not enough disk, or not enough CPU, print an error
+
 if not check_disk_usage('/') or not check_cpu_usage():
     print("ERROR!")
 elif check_localhost() and check_connectivity():
@@ -28,8 +28,8 @@ import socket
 
 def check_localhost():
         localhost = socket.gethostbyname('localhost')
-        return localhost=="127.0.0.1"
+        return localhost == "127.0.0.1"
 
 def check_connectivity():
         request = requests.get("http://www.google.com")
-        return request.status_code== int(200)
+        return request.status_code == int(200)
